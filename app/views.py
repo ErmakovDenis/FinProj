d817a92af941178eafac7caedd2e87f565f40907
ErmakovDenis <clutcherdenis@gmail.com>
2023-01-14 22:23:56 +0500

    предфинальный коммит

--- app/views.py
+++ app/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 
 import math
-
+import billiard as multiprocessing
 import datetime
 import requests
 
 
+import csv
+import threading
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+
 from django.shortcuts import render
-import multiprocessing
+#import multiprocessing
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 # ------------------------ demand
 list_print1 = ['Динамика уровня зарплат по годам: ', 'Динамика количества вакансий по годам: ',
@@ -38,7 +42,7 @@ class Solution:
         data_of_file["year"] = data_of_file["published_at"].apply(lambda x: x[:4])
         data_of_file = data_of_file.groupby("year")
         for year, data in data_of_file:
-            data[["name", "salary_from", "salary_to", "salary_currency", "area_name", "published_at"]].\
+            data[["name", "salary_from", "salary_to", "salary_currency", "area_name", "published_at"]]. \
                 to_csv(rf"templates/data/year_number_{year}.csv", index=False)
 
     def get_dynamics(self):
@@ -88,7 +92,7 @@ class Solution:
 
         self.dynamics6 = dict(zip(data_of_file.head(10)["area_name"], data_of_file.head(10)["count"]))
 
-    def print_statistic(self):
+    def get_statistic(self):
         InputConnect(self.path_to_file, self.name_vacancy, self.dynamics1, self.dynamics2, self.dynamics3,
                      self.dynamics4, self.dynamics5, self.dynamics6)
         return self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6
@@ -100,7 +104,8 @@ class InputConnect:
         self.path_to_file, self.name_vacancy = path_to_file, name_vacancy
         dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6 = dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6
         new_graphic = Report(self.name_vacancy, dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6)
-        new_graphic.generate_image()
+        new_graphic.generate_image_demand()
+        new_graphic.generate_image_geo()
 
 
 def dict_sort(dict):
@@ -121,55 +126,65 @@ class Report:
         self.dynamics5 = dynamics5
         self.dynamics6 = dynamics6
 
-    def generate_image(self):
+    def generate_image_demand(self):
         x = np.arange(len(self.dynamics1.keys()))
         width = 0.35
 
-        fig, axs = plt.subplots(ncols=2, nrows=2)
-        axs[0, 0].bar(x - width / 2, self.dynamics1.values(), width, label='средняя з/п')
-        axs[0, 0].bar(x + width / 2, self.dynamics3.values(), width, label='з/п {0}'.format(self.name_vacancy))
-        plt.rcParams['font.size'] = '8'
-        for label in (axs[0, 0].get_xticklabels() + axs[0, 0].get_yticklabels()):
-            label.set_fontsize(7)
-        axs[0, 0].set_title('Уровень зарплат по годам')
-        axs[0, 0].set_xticks(x, self.dynamics1.keys(), rotation=90)
-        axs[0, 0].grid(axis='y')
-        axs[0, 0].legend(fontsize=7)
 
-        axs[0, 1].bar(x - width / 2, self.dynamics2.values(), width, label='количество вакансий')
-        axs[0, 1].bar(x + width / 2, self.dynamics4.values(), width,
-                      label='количество вакансий {0}'.format(self.name_vacancy))
-        for label in (axs[0, 1].get_xticklabels() + axs[0, 1].get_yticklabels()):
-            label.set_fontsize(7)
-        axs[0, 1].set_title('Количество вакансий по годам')
-        axs[0, 1].set_xticks(x, self.dynamics2.keys(), rotation=90)
-        axs[0, 1].grid(axis='y')
-        axs[0, 1].legend(fontsize=7)
+        fig, axs = plt.subplots(1, 2)
+        axs[0].bar(x - width / 2, self.dynamics1.values(), width, label='средняя з/п')
+        axs[0].bar(x + width / 2, self.dynamics3.values(), width, label='з/п {0}'.format(self.name_vacancy))
+        plt.rcParams['font.size'] = '8'
+        for label in (axs[0].get_xticklabels() + axs[0].get_yticklabels()):
+                label.set_fontsize(7)
+        axs[0].set_title('Уровень зарплат по годам')
+        axs[0].set_xticks(x, self.dynamics1.keys(), rotation=90)
+        axs[0].grid(axis='y')
+        axs[0].legend(fontsize=7)
+
+
+        axs[1].bar(x - width / 2, self.dynamics2.values(), width, label='количество вакансий')
+        axs[1].bar(x + width / 2, self.dynamics4.values(), width,
+                       label='количество вакансий {0}'.format(self.name_vacancy))
+        for label in (axs[1].get_xticklabels() + axs[1].get_yticklabels()):
+                label.set_fontsize(7)
+        axs[1].set_title('Количество вакансий по годам')
+        axs[1].set_xticks(x, self.dynamics2.keys(), rotation=90)
+        axs[1].grid(axis='y')
+        axs[1].legend(fontsize=7)
         fig.tight_layout()
 
+        plt.tight_layout()
+
+        plt.savefig('/home/ermakov/PycharmProjects/FinProject/static/graph2.png', dpi=300)
+
+
+    def generate_image_geo(self):
+        fig, axs = plt.subplots(ncols=2, nrows=1)
+        fig.tight_layout()
         areas = []
         for area in self.dynamics5.keys():
             areas.append(str(area).replace(' ', '\n').replace('-', '-\n'))
         y_pos = np.arange(len(areas))
         performance = self.dynamics5.values()
         error = np.random.rand(len(areas))
-        axs[1, 0].barh(y_pos, performance, xerr=error, align='center')
-        for label in (axs[1, 0].get_xticklabels() + axs[1, 0].get_yticklabels()):
+        axs[0].barh(y_pos, performance, xerr=error, align='center')
+        for label in (axs[0].get_xticklabels() + axs[0].get_yticklabels()):
             label.set_fontsize(7)
-        axs[1, 0].set_yticks(y_pos, labels=areas, size=7)
-        axs[1, 0].invert_yaxis()
-        axs[1, 0].grid(axis='x')
-        axs[1, 0].set_title('Уровень зарплат по городам')
+        axs[0].set_yticks(y_pos, labels=areas, size=7)
+        axs[0].invert_yaxis()
+        axs[0].grid(axis='x')
+        axs[0].set_title('Уровень зарплат по городам')
+
 
         val = list(self.dynamics6.values()) + [1 - sum(list(self.dynamics6.values()))]
         k = list(self.dynamics6.keys()) + ['Другие']
-        axs[1, 1].pie(val, labels=k, startangle=150)
-        axs[1, 1].set_title('Доля вакансий по городам')
+        axs[1].pie(val, labels=k, startangle=150)
+        axs[1].set_title('Доля вакансий по городам')
 
         plt.tight_layout()
-        plt.savefig('/home/ermakov/PycharmProjects/FinProject/static/graph2.png', dpi=300)
 
-
+        plt.savefig('/home/ermakov/PycharmProjects/FinProject/static/graph3.png', dpi=300)
 
 
 def demand(request):
@@ -178,13 +193,12 @@ def demand(request):
     solve = Solution(filename, name_vacancy)
     solve.split_by_year()
     solve.get_dynamics()
-    dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6 = solve.print_statistic()
+    dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6 = solve.get_statistic()
     dynamics = []
 
     for year in dynamics2.keys():
         dynamics.append([year, dynamics1[year], dynamics2[year], dynamics3[year], dynamics4[year]])
-    for key in dynamics6:
-        dynamics6[key] = round(dynamics6[key] * 100, 2)
+
 
     dynamics.sort()
 
@@ -211,25 +225,90 @@ def demand(request):
             'val18': dynamics[18],
             'val19': dynamics[19],
             "dinamics": dynamics,
-            'dynamics5': dynamics5.items(),
-            'dynamics6': dynamics6.items()
             }
 
     return render(request, "demand.html", context=data)
 
-
-# ------------------------- end_demand
 
 def main(request):
     return render(request, "main.html")
 
 
 def geography(request):
-    return render(request, "geography.html")
+    filename = '/home/ermakov/PycharmProjects/FinProject/templates/data/vacancies_with_skills.csv'
+    name_vacancy = "Backend"
+    solve = Solution(filename, name_vacancy)
+    solve.get_dynamics()
+    dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6 = solve.get_statistic()
+    for key in dynamics6:
+        dynamics6[key] = round(dynamics6[key] * 100, 2)
+    data = {
+        'dynamics5': dynamics5.items(),
+        'dynamics6': dynamics6.items(),
+    }
+    return render(request, "geography.html", context=data)
+#----------------------------------------
+
+
+
+vacancy_choices = '|'.join(['backend'])
+data = pd.read_csv('/home/ermakov/PycharmProjects/FinProject/templates/data/vacancies_with_skills.csv', usecols=['name', 'key_skills', 'published_at'])
+
+data = data[data['name'].str.contains(vacancy_choices,case=False)]
+data = data[data['key_skills'].notnull()]
+data['year'] = data['published_at'].apply(lambda x: x[:4])
+groups = data.groupby(['year'])
+
+
+def write_group_to_csv(group):
+    year, data = group
+    data.to_csv(f'skills_by_city/key_skills_by_{year}.csv', index=False)
+
+
+thread_pool = []
+
+
+for group in groups:
+	thread = threading.Thread(target=write_group_to_csv, args=(group,))
+	thread.start()
+	thread_pool.append(thread)
+
+
+for thread in thread_pool:
+	thread.join()
+
+
+class Skills:
+	def __init__(self, name, amount):
+		self.name = name
+		self.amount = amount
+
+	def __repr__(self):
+		return f'{self.name}: {self.amount}'
+
+
+def analyze_year(file_name):
+    all_skills = []
+    with open(file_name, 'r', encoding='utf-8-sig') as skills:
+        rows = csv.reader(skills)
+        next(rows)
+        for row in rows:
+            s = row[1]
+            for i in s.split('\n'):
+                all_skills.append(i)
+    DATA = pd.DataFrame({'skills': all_skills}).value_counts().head(10).to_dict()
+    date = file_name[-8:-4]
+    return date, [Skills(skill[0], amount) for skill, amount in DATA.items()]
+
+
 
 
 def skills(request):
-    return render(request, "skills.html")
+    file_names = [f'skills_by_city/{file}' for file in os.listdir('skills_by_city')]
+    with ProcessPoolExecutor(max_workers=cpu_count()) as ex:
+        res = ex.map(analyze_year, file_names)
+        data = {"res": sorted(res)}
+    return render(request, "skills.html", context=data)
 
 
 #------------------------
